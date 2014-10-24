3a32448fb3063b38a3680ec35424bee3d7421326
Mike Mackintosh <m@zyp.io>
2014-10-24 12:38:19 -0400

    updated hidden directories

--- providers/hide_directories.rb
+++ providers/hide_directories.rb
@@ -1,5 +1,10 @@
 action :hide do
   new_resource.directories.each do |dir| 
+    
+    if dir.include?('~/')
+      dir.gsub('~/', node['system']['home'])
+    end
+
     execute "Hiding directory: #{dir}" do
       command "chflags hidden '#{dir}'"
       user node['system']['current_user']
@@ -9,6 +14,11 @@ end
 
 action :show do
   new_resource.directories.each do |dir| 
+    
+    if dir.include?('~/')
+      dir.gsub('~/', node['system']['home'])
+    end
+    
     execute "Unhiding directory: #{dir}" do
       command "chflags nohidden '#{dir}'"
       user node['system']['current_user']
