d0f4869e0198b4429d9d9de6d79bc313c3486d32
Mike Mackintosh <m@zyp.io>
2014-10-24 12:24:39 -0400

    updated hidden directories

--- resources/hide_directories.rb
+++ resources/hide_directories.rb
@@ -1,7 +1,7 @@
 actions :hide, :create
 default_action :hide
 
-attribute :directory, :kind_of => [Array, Hash], :name_attribute => true
+attribute :directories, :kind_of => [Array, Hash], :name_attribute => true
 
 def initialize(name, run_context=nil)
   super
