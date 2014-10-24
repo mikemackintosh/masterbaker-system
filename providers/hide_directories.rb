ad2afd1f2c1fa44bd8b322419c7a9b577df1b366
Mike Mackintosh <m@zyp.io>
2014-10-24 12:53:57 -0400

    updated hidden directories

--- providers/hide_directories.rb
+++ providers/hide_directories.rb
@@ -1,26 +1,23 @@
 action :hide do
   new_resource.directories.each do |dir| 
-    
-    if dir.include?('~/')
-      throw dir.gsub('~/', node['system']['home'])
-    end
-
-    execute "Hiding directory: #{dir}" do
-      command "chflags hidden '#{dir}'"
-      user node['system']['current_user']
-    end
+    toggle_flag(dir, "hidden")
   end
 end
 
 action :show do
   new_resource.directories.each do |dir| 
-    
+    toggle_flag(dir, "nohidden")
+  end
+end
+
+def toggle_flag(dir, flag)
+  unless dir.nil?
     if dir.include?('~/')
       throw dir.gsub('~/', node['system']['home'])
     end
     
     execute "Unhiding directory: #{dir}" do
-      command "chflags nohidden '#{dir}'"
+      command "chflags #{flag} '#{dir}'"
       user node['system']['current_user']
     end
   end
