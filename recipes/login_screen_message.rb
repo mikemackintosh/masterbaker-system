5b20406a45bac8c12baca2c2a497fad258f6ebac
Mike Mackintosh <m@zyp.io>
2014-10-26 16:08:13 -0400

    testing something

--- recipes/login_screen_message.rb
+++ recipes/login_screen_message.rb
@@ -1,9 +1,8 @@
 #unless node['system']['login_window_message'].nil?
   mac_os_x_userdefaults "Setting login screen message" do
     domain "/Library/Preferences/com.apple.loginwindow"
-    global true
     key "LoginWindowText"
     type "string"
-    value node['system']['login_window_message']
+    value node['system']['login_window_message']['login_window_message']
   end
 #end
