a26c0bac2c8f08bd87d7fc3778c63b960acd10f4
Mike Mackintosh <m@zyp.io>
2014-10-26 16:33:58 -0400

    added changelog to metadata, v0.2.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,13 @@ maintainer_email "m@splug.io"
 license          "Apache 2.0"
 description      "Manage System Stuff for OS X"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.2"
+version          "0.2.3"
 
 depends "homebrew"
 depends 'mac_os_x'
+
+# 0.2.3
+# - Added fix for login window message
+#
+# <= 0.2.2
+# - Bunch of stuff
