1634bc72d20c0860c1dccab03bd43b6d202d5fd3
Mike Mackintosh <m@zyp.io>
2014-10-27 11:11:15 -0400

    login window plist key change

--- recipes/login_screen_message.rb
+++ recipes/login_screen_message.rb
@@ -1,7 +1,7 @@
 #unless node['system']['login_window_message'].nil?
   mac_os_x_userdefaults "Setting login screen message" do
     domain "/Library/Preferences/com.apple.loginwindow"
-    key "LoginWindowText"
+    key "LoginwindowText"
     type "string"
     value node['system']['login_window_message']['login_window_message']
   end
