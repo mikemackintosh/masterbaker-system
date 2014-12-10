9a13976ac467625edcb8259fc290d9503bb6e931
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 15:29:42 -0500

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,12 +4,13 @@ include_recipe "system::timemachine"
 include_recipe "system::system_save_dialogs"
 include_recipe "system::toggle_lightdark_mode"
 
-#mac_os_x_userdefaults "Disable swipe between pages" do
-#  domain 'NSGlobalDomain'
-#  global true
-#  key "AppleEnableSwipeNavigateWithScrolls"
-#  value 0
-#end
+mac_os_x_userdefaults "Disable swipe between pages" do
+  domain 'NSGlobalDomain'
+  global true
+  key "AppleEnableSwipeNavigateWithScrolls"
+  type "bool"
+  value "false"
+ #end
 
 mac_os_x_userdefaults "Don't create .DS_store on network volumes" do
   domain 'com.apple.desktopservices'
